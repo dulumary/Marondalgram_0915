ce3a1f7955ead7cbe19c6d99f82889541209bda0
mega_202 <mega>
2023-11-14 18:10:19 +0900

    [김인규]  - 수정 테스트

--- src/main/java/com/marondal/marondalgram/config/WebMvcConfig.java
+++ src/main/java/com/marondal/marondalgram/config/WebMvcConfig.java
@@ -13,6 +13,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/images/**")
 		.addResourceLocations("file:///" + FileManager.FILE_UPLOAD_PATH + "/");
+		
+		
 	}
 	
 	
